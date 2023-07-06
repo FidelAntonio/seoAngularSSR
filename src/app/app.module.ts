cc57857b053bd5509941d4053f13be68daaae4a6
Israel Fidel <66265012+FidelAntonio@users.noreply.github.com>
2023-07-06 16:42:19 -0600

    Inicio

---
+++ src/app/app.module.ts
@@ -0,0 +1,50 @@
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { RouterLink, RouterModule } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { DetalleComponent } from './pages/detalle/detalle.component';
+import { Observable, tap } from 'rxjs';
+import { response } from 'express';
+
+
+function initializeAppFactory(httpClient: HttpClient): () => Observable<any> {
+  return () => httpClient.get("https://rickandmortyapi.com/api/character/1")
+    .pipe(
+        tap(user => {
+          console.log(user);
+         })
+      // console.log(response)
+    );
+ }
+
+
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    DetalleComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserModule,
+    RouterModule
+  ],
+  providers: [{
+    provide: APP_INITIALIZER,
+    useFactory: initializeAppFactory,
+    deps: [HttpClient],
+    multi: true
+  }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/pages/detalle/detalle.component.ts
@@ -0,0 +1,86 @@
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-detalle',
+  templateUrl: './detalle.component.html',
+  styleUrls: ['./detalle.component.scss']
+})
+export class DetalleComponent implements OnInit {
+
+  // ulrimg="http://webdiputados/{titujloentrada}.png";
+  urlimagen = 'https://rickandmortyapi.com/api/character/avatar/'
+
+
+  constructor(
+    private httpService: HttpClient,
+    private url: ActivatedRoute,
+    private meta: Meta,
+    private title: Title,
+    @Inject(PLATFORM_ID) private platformId: Object,
+    @Inject(DOCUMENT) private doc: any,
+  ) {
+
+    let id: any = ''
+    let name: any = ''
+    if (isPlatformBrowser(this.platformId)) {
+      let url = window.location.search;
+      const params = new URLSearchParams(url);
+      id = params.get('id')
+      name = params.get('name')
+    }
+    this.setSeo(name, 'https://rickandmortyapi.com/api/character/avatar/' + id + '.jpeg')
+  }
+
+  result: any = []
+  id: string = '';
+  name: string = '';
+  ngOnInit(): void {
+    this.url.queryParams.subscribe(async (e: any) => {
+      this.id = e.id;
+      this.name = e.name;
+
+
+      //   this.meta.addTag({name: 'robots', content:'noindex'})
+      const linkElement = this.doc.head.querySelector(`link[rel='canonical']`)
+        || this.doc.head.appendChild(this.doc.createElement('link'));
+
+      if (linkElement) {
+        linkElement.setAttribute('rel', 'canonical');
+        linkElement.setAttribute('href', this.doc.URL.split('?')[0]);
+      }
+    });
+
+    setTimeout(() => {
+      this.setSeo(this.name, 'https://rickandmortyapi.com/api/character/avatar/' + this.id + '.jpeg')
+    }, 4000);
+    this.getInfoPersonaje();
+  }
+
+  ngOnDestroy(): void {
+    this.meta.removeTag('name="robots"')
+  }
+
+  setSeo(title: any = 'Sin titulo', image: any) {
+    let ima2 = 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+
+
+  }
+
+  async getInfoPersonaje() {
+    await this.httpService.get('https://rickandmortyapi.com/api/character/' + this.id).subscribe((result: any) => {
+      this.result = result;
+      console.log('h', result);
+      this.title.setTitle(this.result.name + ' | SEO dinamico')
+      this.meta.updateTag({ property: 'description', content: `${this.result.image}` });
+      this.meta.updateTag({ property: 'og:title', content: this.result.name + ' | SeoDinamico' });
+      this.meta.updateTag({ property: 'og:image', content: this.result.image });
+
+
+    })
+  }
+
+}
