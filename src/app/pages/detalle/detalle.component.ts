2f7252c6e589db7c7d3f8f5f75ace9da87b38fa1
Israel Fidel <66265012+FidelAntonio@users.noreply.github.com>
2023-07-10 17:35:27 -0600

    navigation end

--- src/app/pages/detalle/detalle.component.ts
+++ src/app/pages/detalle/detalle.component.ts
@@ -13,9 +13,10 @@ import { filter, map, mergeMap, tap } from 'rxjs';
 export class DetalleComponent implements OnInit {
   // ulrimg="http://webdiputados/{titujloentrada}.png";
   urlimagen = 'https://rickandmortyapi.com/api/character/avatar/'
-
+  previousUrl:any;
 
   constructor(
+    private router: Router,
     private httpService: HttpClient,
     private url: ActivatedRoute,
     private meta: Meta,
@@ -23,41 +24,56 @@ export class DetalleComponent implements OnInit {
     @Inject(PLATFORM_ID) private platformId: Object,
     @Inject(DOCUMENT) private doc: any,
   ) {
+    router.events
+    .pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe((event: any) => {
+      console.log('prev:', event);
+      this.previousUrl = event.url;
+      this.httpService.get('https://rickandmortyapi.com/api/character/' + event.id).subscribe((result: any) => {
+      this.result = result;
+      console.log('h', result);
+      this.title.setTitle(this.result.name + ' | SEO dinamico')
+      this.meta.updateTag({ property: 'description', content: `${this.result.image}` });
+      this.meta.updateTag({ property: 'og:title', content: this.result.name + ' | SeoDinamico' });
+      this.meta.updateTag({ property: 'og:image', content: this.result.image });
 
-    let id: any = ''
-    let name: any = ''
-    if (isPlatformBrowser(this.platformId)) {
-      let url = window.location.search;
-      const params = new URLSearchParams(url);
-      id = params.get('id')
-      name = params.get('name')
-    }
-    this.setSeo(name, 'https://rickandmortyapi.com/api/character/avatar/' + id + '.jpeg')
+
+    })
+    });
+    // let id: any = ''
+    // let name: any = ''
+    // if (isPlatformBrowser(this.platformId)) {
+    //   let url = window.location.search;
+    //   const params = new URLSearchParams(url);
+    //   id = params.get('id')
+    //   name = params.get('name')
+    // }
+    // this.setSeo(name, 'https://rickandmortyapi.com/api/character/avatar/' + id + '.jpeg')
   }
 
   result: any = []
   id: string = '';
   name: string = '';
   ngOnInit(): void {
-    this.url.queryParams.subscribe(async (e: any) => {
-      this.id = e.id;
-      this.name = e.name;
+    // this.url.queryParams.subscribe(async (e: any) => {
+    //   this.id = e.id;
+    //   this.name = e.name;
 
 
-      //   this.meta.addTag({name: 'robots', content:'noindex'})
-      const linkElement = this.doc.head.querySelector(`link[rel='canonical']`)
-        || this.doc.head.appendChild(this.doc.createElement('link'));
+    //   //   this.meta.addTag({name: 'robots', content:'noindex'})
+    //   const linkElement = this.doc.head.querySelector(`link[rel='canonical']`)
+    //     || this.doc.head.appendChild(this.doc.createElement('link'));
 
-      if (linkElement) {
-        linkElement.setAttribute('rel', 'canonical');
-        linkElement.setAttribute('href', this.doc.URL.split('?')[0]);
-      }
-    });
+    //   if (linkElement) {
+    //     linkElement.setAttribute('rel', 'canonical');
+    //     linkElement.setAttribute('href', this.doc.URL.split('?')[0]);
+    //   }
+    // });
 
-    setTimeout(() => {
-      this.setSeo(this.name, 'https://rickandmortyapi.com/api/character/avatar/' + this.id + '.jpeg')
-    }, 4000);
-    this.getInfoPersonaje();
+    // setTimeout(() => {
+    //   this.setSeo(this.name, 'https://rickandmortyapi.com/api/character/avatar/' + this.id + '.jpeg')
+    // }, 4000);
+    // this.getInfoPersonaje();
   }
 
   ngOnDestroy(): void {
@@ -73,11 +89,11 @@ export class DetalleComponent implements OnInit {
   async getInfoPersonaje() {
     await this.httpService.get('https://rickandmortyapi.com/api/character/' + this.id).subscribe((result: any) => {
       this.result = result;
-      console.log('h', result);
-      this.title.setTitle(this.result.name + ' | SEO dinamico')
-      this.meta.updateTag({ property: 'description', content: `${this.result.image}` });
-      this.meta.updateTag({ property: 'og:title', content: this.result.name + ' | SeoDinamico' });
-      this.meta.updateTag({ property: 'og:image', content: this.result.image });
+      // console.log('h', result);
+      // this.title.setTitle(this.result.name + ' | SEO dinamico')
+      // this.meta.updateTag({ property: 'description', content: `${this.result.image}` });
+      // this.meta.updateTag({ property: 'og:title', content: this.result.name + ' | SeoDinamico' });
+      // this.meta.updateTag({ property: 'og:image', content: this.result.image });
 
 
     })
