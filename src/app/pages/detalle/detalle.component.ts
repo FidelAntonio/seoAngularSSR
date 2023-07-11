4e65d386d42f211549c3053ba68c4ec784b14185
Israel Fidel <66265012+FidelAntonio@users.noreply.github.com>
2023-07-11 14:45:50 -0600

    do checked

--- src/app/pages/detalle/detalle.component.ts
+++ src/app/pages/detalle/detalle.component.ts
@@ -1,6 +1,12 @@
 import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { AfterViewInit, Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  Inject,
+  OnInit,
+  PLATFORM_ID,
+} from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { filter, map, mergeMap, tap } from 'rxjs';
@@ -8,12 +14,12 @@ import { filter, map, mergeMap, tap } from 'rxjs';
 @Component({
   selector: 'app-detalle',
   templateUrl: './detalle.component.html',
-  styleUrls: ['./detalle.component.scss']
+  styleUrls: ['./detalle.component.scss'],
 })
 export class DetalleComponent implements AfterViewInit, OnInit {
   // ulrimg="http://webdiputados/{titujloentrada}.png";
-  urlimagen = 'https://rickandmortyapi.com/api/character/avatar/'
-  previousUrl:any;
+  urlimagen = 'https://rickandmortyapi.com/api/character/avatar/';
+  previousUrl: any;
 
   constructor(
     private router: Router,
@@ -22,24 +28,33 @@ export class DetalleComponent implements AfterViewInit, OnInit {
     private meta: Meta,
     private title: Title,
     @Inject(PLATFORM_ID) private platformId: Object,
-    @Inject(DOCUMENT) private dom: any,
+    @Inject(DOCUMENT) private dom: any
   ) {
     router.events
-    .pipe(filter(event => event instanceof NavigationEnd))
-    .subscribe((event: any) => {
-      console.log('prev:', event);
-      this.previousUrl = event.url;
-      this.httpService.get('https://rickandmortyapi.com/api/character/' + event.id).subscribe((result: any) => {
-      this.result = result;
-      console.log('h', result);
-      this.title.setTitle(this.result.name + ' | SEO dinamico')
-      this.meta.updateTag({ property: 'description', content: `${this.result.image}` });
-      this.meta.updateTag({ property: 'og:title', content: this.result.name + ' | SeoDinamico' });
-      this.meta.updateTag({ property: 'og:image', content: this.result.image });
-
-
-    })
-    });
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        console.log('prev:', event);
+        this.previousUrl = event.url;
+        this.httpService
+          .get('https://rickandmortyapi.com/api/character/' + event.id)
+          .subscribe((result: any) => {
+            this.result = result;
+            console.log('h', result);
+            this.title.setTitle(this.result.name + ' | SEO dinamico');
+            this.meta.updateTag({
+              property: 'description',
+              content: `${this.result.image}`,
+            });
+            this.meta.updateTag({
+              property: 'og:title',
+              content: this.result.name + ' | SeoDinamico',
+            });
+            this.meta.updateTag({
+              property: 'og:image',
+              content: this.result.image,
+            });
+          });
+      });
     // let id: any = ''
     // let name: any = ''
     // if (isPlatformBrowser(this.platformId)) {
@@ -51,7 +66,7 @@ export class DetalleComponent implements AfterViewInit, OnInit {
     // this.setSeo(name, 'https://rickandmortyapi.com/api/character/avatar/' + id + '.jpeg')
   }
 
-  result: any = []
+  result: any = [];
   id: string = '';
   name: string = '';
   ngOnInit(): void {
@@ -60,7 +75,6 @@ export class DetalleComponent implements AfterViewInit, OnInit {
     // this.url.queryParams.subscribe(async (e: any) => {
     //   this.id = e.id;
     //   this.name = e.name;
-
 
     //   //   this.meta.addTag({name: 'robots', content:'noindex'})
     //   const linkElement = this.doc.head.querySelector(`link[rel='canonical']`)
@@ -83,39 +97,52 @@ export class DetalleComponent implements AfterViewInit, OnInit {
   }
 
   ngOnDestroy(): void {
-    this.meta.removeTag('name="robots"')
+    this.meta.removeTag('name="robots"');
   }
 
   setSeo(title: any = 'Sin titulo', image: any) {
-    let ima2 = 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
-
-
+    let ima2 = 'https://rickandmortyapi.com/api/character/avatar/3.jpeg';
   }
 
   async getInfoPersonaje() {
-    await this.httpService.get('https://rickandmortyapi.com/api/character/' + this.id).subscribe((result: any) => {
-      this.result = result;
-      // console.log('h', result);
-      // this.title.setTitle(this.result.name + ' | SEO dinamico')
-      // this.meta.updateTag({ property: 'description', content: `${this.result.image}` });
-      // this.meta.updateTag({ property: 'og:title', content: this.result.name + ' | SeoDinamico' });
-      // this.meta.updateTag({ property: 'og:image', content: this.result.image });
-
-
-    })
+    await this.httpService
+      .get('https://rickandmortyapi.com/api/character/' + this.id)
+      .subscribe((result: any) => {
+        this.result = result;
+        // console.log('h', result);
+        // this.title.setTitle(this.result.name + ' | SEO dinamico')
+        // this.meta.updateTag({ property: 'description', content: `${this.result.image}` });
+        // this.meta.updateTag({ property: 'og:title', content: this.result.name + ' | SeoDinamico' });
+        // this.meta.updateTag({ property: 'og:image', content: this.result.image });
+      });
   }
   createCanonicalURL() {
     let link: HTMLLinkElement = this.dom.createElement('link');
     link.setAttribute('rel', 'canonical');
     this.dom.head.appendChild(link);
     link.setAttribute('href', this.dom.URL);
- }
- updateMetaTags() {
-  this.meta.updateTag({ property: 'og:image', content: this.result.image }, "property='og:image'");
-  this.meta.updateTag({ property: 'og:title', content: this.result.name }, "property='og:title'");
-  this.meta.updateTag({ property: 'og:description', content: this.result.species }, "property='og:description'");
-}
-ngAfterViewInit(){
-  this.updateMetaTags();
-}
+  }
+  updateMetaTags() {
+    this.meta.updateTag(
+      { property: 'og:image', content: this.result.image },
+      "property='og:image'"
+    );
+    this.meta.updateTag(
+      { property: 'og:title', content: this.result.name },
+      "property='og:title'"
+    );
+    this.meta.updateTag(
+      { property: 'og:description', content: this.result.species },
+      "property='og:description'"
+    );
+  }
+  ngAfterViewInit() {
+    this.updateMetaTags();
+  }
+  ngDoCheck() {
+    console.log('do check');
+  }
+  ngAfterViewChecked() {
+    console.log('after view checked');
+  }
 }
