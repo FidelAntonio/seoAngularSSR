6e96a577cdb90ece98b530e1e1cb0ff3df4210c4
Israel Fidel <66265012+FidelAntonio@users.noreply.github.com>
2023-07-12 10:39:38 -0600

    API TRANSLATE

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,43 +1,79 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
+import {
+  GoogleObj,
+  GoogletranslateService,
+} from 'src/app/services/googletranslate.service';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.scss']
+  styleUrls: ['./inicio.component.scss'],
 })
-export class InicioComponent implements OnInit{
-
+export class InicioComponent implements OnInit {
   constructor(
     private httpService: HttpClient,
     private metaService: Meta,
-    private title: Title
-  ){
-    this.metaService.removeTag( 'name="robots"' )
-    this.title.setTitle('Listado | SEO dinamico')
-    this.metaService.updateTag({ property: 'og:title', content: 'Listado Personajes | SeoDinamico' });
-    this.metaService.updateTag({ property: 'description', content: 'Listado Personajes desde la API | SeoDinamico' });
+    private title: Title,
+    private google: GoogletranslateService
+  ) {
+    this.metaService.removeTag('name="robots"');
+    this.title.setTitle('Listado | SEO dinamico');
+    this.metaService.updateTag({
+      property: 'og:title',
+      content: 'Listado Personajes | SeoDinamico',
+    });
+    this.metaService.updateTag({
+      property: 'description',
+      content: 'Listado Personajes desde la API | SeoDinamico',
+    });
     // this.metaService.updateTag({ property: 'og:url', content: 'Listado Personajes | SeoDinamico' });
-    this.metaService.updateTag({ property: 'og:image', content: 'https://media.revistagq.com/photos/640060bc0350668344429c1b/master/pass/42133433-01B0-4FF7-B6F1-3CF554564B76.jpeg' });
-
+    this.metaService.updateTag({
+      property: 'og:image',
+      content:
+        'https://media.revistagq.com/photos/640060bc0350668344429c1b/master/pass/42133433-01B0-4FF7-B6F1-3CF554564B76.jpeg',
+    });
   }
 
-  result: any =[]
-
+  result: any = [];
+  translateBtn: any;
   ngOnInit(): void {
     // let max = 42
     // let min = 0
     // let num = Math.floor(Math.random()*(max - min)+min);
     // console.log(num)
-    this.getInfoPersonajes()
+    this.getInfoPersonajes();
+
   }
 
-
-
-  getInfoPersonajes(){
-   this.httpService.get('https://rickandmortyapi.com/api/character/?page=1').subscribe((result: any)=>{
-     this.result = result.results;
-   })
+  getInfoPersonajes() {
+    this.httpService
+      .get('https://rickandmortyapi.com/api/character/?page=1')
+      .subscribe((result: any) => {
+        this.result = result.results;
+      });
+  }
+  send() {
+    const googleObj: GoogleObj = {
+      q: [
+        'he Bundestag (German pronunciation: [ˈbʊndəstaːk] (listen), "Federal Diet") is the German federal parliament. It is the only federal representative body ',
+      'this is an example from js node'],
+      target: 'es',
+    };
+    this.translateBtn.disabled = true;
+    this.google.translate(googleObj).subscribe(
+      (res: any) => {
+        this.translateBtn.disabled = false;
+        console.log(res.data.translations[0].translatedText);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+  ngAfterViewChecked() {
+    this.translateBtn = document.getElementById('translatebtn');
+    console.log(this.translateBtn);
   }
 }
---
+++ src/app/services/googletranslate.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+export interface GoogleObj {
+  q: string[];
+  target: string;
+  }
+@Injectable({
+  providedIn: 'root'
+})
+export class GoogletranslateService {
+  url = 'https://translation.googleapis.com/language/translate/v2?key=';
+  key = 'AIzaSyA0WpuZC_xZhClqZScuwrw3xLIVvZwru_Y';
+  constructor(private http: HttpClient) { }
+  translate(obj: GoogleObj) {
+    return this.http.post(this.url + this.key, obj);
+    }
+}
