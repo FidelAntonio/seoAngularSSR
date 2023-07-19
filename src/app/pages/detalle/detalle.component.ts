b4b3236b62957adeb97cab65f793d50e66f7d718
Israel Fidel <66265012+FidelAntonio@users.noreply.github.com>
2023-07-19 10:09:24 -0600

    ngDestroy

--- src/app/pages/detalle/detalle.component.ts
+++ src/app/pages/detalle/detalle.component.ts
@@ -17,73 +17,135 @@ import { filter, map, mergeMap, tap } from 'rxjs';
   styleUrls: ['./detalle.component.scss'],
 })
 export class DetalleComponent implements  OnInit {
- // ulrimg="http://webdiputados/{titujloentrada}.png";
- image_url = 'https://rickandmortyapi.com/api/character/avatar/1.jpeg';
- page_title="'Hola!!!";
+  // ulrimg="http://webdiputados/{titujloentrada}.png";
+  urlimagen = 'https://rickandmortyapi.com/api/character/avatar/';
+  previousUrl: any;
 
- constructor(
-   private httpService: HttpClient,
-   private url: ActivatedRoute,
-   private meta: Meta,
-   private title: Title,
-   @Inject(PLATFORM_ID) private platformId: Object,
-   @Inject(DOCUMENT) private doc: any,
- ) {
+  constructor(
+    private router: Router,
+    private httpService: HttpClient,
+    private url: ActivatedRoute,
+    private meta: Meta,
+    private title: Title,
+    @Inject(PLATFORM_ID) private platformId: Object,
+    @Inject(DOCUMENT) private dom: any
+  ) {
+    router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        // console.log('prev:', event);
+        this.previousUrl = event.url;
+        this.httpService
+          .get('https://rickandmortyapi.com/api/character/' + event.id)
+          .subscribe((result: any) => {
+            this.result = result;
+            // console.log('h', result);
+            this.title.setTitle(this.result.name + ' | SEO dinamico');
+            this.meta.updateTag({
+              property: 'description',
+              content: `${this.result.image}`,
+            });
+            this.meta.updateTag({
+              property: 'og:title',
+              content: this.result.name + ' | SeoDinamico',
+            });
+            this.meta.updateTag({
+              property: 'og:image',
+              content: this.result.image,
+            });
+          });
+      });
+    // let id: any = ''
+    // let name: any = ''
+    // if (isPlatformBrowser(this.platformId)) {
+    //   let url = window.location.search;
+    //   const params = new URLSearchParams(url);
+    //   id = params.get('id')
+    //   name = params.get('name')
+    // }
+    // this.setSeo(name, 'https://rickandmortyapi.com/api/character/avatar/' + id + '.jpeg')
+  }
 
-   let id: any = ''
-   let name: any = ''
-   if (isPlatformBrowser(this.platformId)) {
-     let url = window.location.search;
-     const params = new URLSearchParams(url);
-     id = params.get('id')
-     name = params.get('name')
-   }
+  result: any = [];
+  id: string = '';
+  name: string = '';
+  ngOnInit(): void {
+    this.updateMetaTags();
 
- }
+    // this.url.queryParams.subscribe(async (e: any) => {
+    //   this.id = e.id;
+    //   this.name = e.name;
 
- result: any = []
- id: string = '';
- name: string = '';
- ngOnInit(): void {
-   this.url.queryParams.subscribe(async (e: any) => {
-     this.id = e.id;
-     this.name = e.name;
+    //   //   this.meta.addTag({name: 'robots', content:'noindex'})
+    //   const linkElement = this.doc.head.querySelector(`link[rel='canonical']`)
+    //     || this.doc.head.appendChild(this.doc.createElement('link'));
 
+    //   if (linkElement) {
+    //     linkElement.setAttribute('rel', 'canonical');
+    //     linkElement.setAttribute('href', this.doc.URL.split('?')[0]);
+    //   }
+    // });
 
-     //   this.meta.addTag({name: 'robots', content:'noindex'})
-     const linkElement = this.doc.head.querySelector(`link[rel='canonical']`)
-       || this.doc.head.appendChild(this.doc.createElement('link'));
+    // setTimeout(() => {
+    //   this.setSeo(this.name, 'https://rickandmortyapi.com/api/character/avatar/' + this.id + '.jpeg')
+    // }, 4000);
+    // this.getInfoPersonaje();
+    this.createCanonicalURL();
+    // setTimeout(() => {
+    //   this.updateMetaTags();
+    // }, 1000);
+  }
 
-     if (linkElement) {
-       linkElement.setAttribute('rel', 'canonical');
-       linkElement.setAttribute('href', this.doc.URL.split('?')[0]);
-     }
-     this.setSeo(this.name, 'https://rickandmortyapi.com/api/character/avatar/' + this.id + '.jpeg')
-   });
+  ngOnDestroy(): void {
+    this.meta.removeTag('name="robots"');
+    this.createCanonicalURL();
+  }
 
+  setSeo(title: any = 'Sin titulo', image: any) {
+    let ima2 = 'https://rickandmortyapi.com/api/character/avatar/3.jpeg';
+  }
 
-   this.getInfoPersonaje();
- }
+  async getInfoPersonaje() {
+    await this.httpService
+      .get('https://rickandmortyapi.com/api/character/' + this.id)
+      .subscribe((result: any) => {
+        this.result = result;
+        // console.log('h', result);
+        // this.title.setTitle(this.result.name + ' | SEO dinamico')
+        // this.meta.updateTag({ property: 'description', content: `${this.result.image}` });
+        // this.meta.updateTag({ property: 'og:title', content: this.result.name + ' | SeoDinamico' });
+        // this.meta.updateTag({ property: 'og:image', content: this.result.image });
+      });
+  }
+  createCanonicalURL() {
+    let link: HTMLLinkElement = this.dom.createElement('link');
+    link.setAttribute('rel', 'canonical');
+    this.dom.head.appendChild(link);
+    link.setAttribute('href', this.dom.URL);
+  }
+  updateMetaTags() {
+    this.meta.updateTag(
+      { property: 'og:image', content: this.result.image },
+      "property='og:image'"
+    );
+    this.meta.updateTag(
+      { property: 'og:title', content: this.result.name },
+      "property='og:title'"
+    );
+    this.meta.updateTag(
+      { property: 'og:description', content: this.result.species },
+      "property='og:description'"
+    );
+  }
+  ngAfterViewInit() {
+    this.updateMetaTags();
+  }
+  ngDoCheck() {
+    this.updateMetaTags();
 
- ngOnDestroy(): void {
-   this.meta.removeTag('name="robots"')
- }
+  }
+  ngAfterViewChecked() {
+    this.updateMetaTags();
 
- setSeo(title: any = 'Sin titulo', image: any) {
-   let ima2 = 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
- }
-
- async getInfoPersonaje() {
-   await this.httpService.get('https://rickandmortyapi.com/api/character/' + this.id).subscribe((result: any) => {
-     this.result = result;
-     console.log('h', result);
-     this.title.setTitle(this.result.name + ' | SEO dinamico')
-     this.meta.updateTag({ property: 'description', content: `${this.result.image}` });
-     this.meta.updateTag({ property: 'og:description', content: `Detalle de ${result.name}` });
-     this.meta.updateTag({ property: 'og:title', content: this.result.name + ' | SeoDinamico' });
-     this.meta.updateTag({ property: 'og:image', content: this.result.image });
-     this.meta.updateTag({ property: 'twitter:image', content: this.result.image });
-   })
- }
-
+  }
 }
